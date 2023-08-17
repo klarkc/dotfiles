959a29411a12f4a911e03f604bd80d653e444587
klarkc <walkerleite490@gmail.com>
2023-08-17 16:53:38 -0300

    feat(xmonad): add system shortcuts

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -1,13 +1,16 @@
+import Graphics.X11.ExtraTypes (xF86XK_PowerDown)
 import XMonad
   ( Full (Full),
     Mirror (Mirror),
     Tall (Tall),
-    XConfig (terminal),
+    XConfig (keys, terminal),
+    handleEventHook,
     layoutHook,
     spawn,
     xmonad,
-    (|||),
+    (|||), Default (def),
   )
+import XMonad.Actions.ShowText (handleTimerEvent, flashText)
 import XMonad.Config.Desktop (desktopConfig)
 import XMonad.Hooks.DynamicLog (xmobarProp)
 import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
@@ -15,8 +18,9 @@ import XMonad.Hooks.StatusBar ()
 import XMonad.Hooks.StatusBar.PP ()
 import XMonad.Layout.Magnifier (magnifiercz')
 import XMonad.Layout.ThreeColumns (ThreeCol (ThreeColMid))
-import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.EZConfig (additionalKeys, additionalKeysP)
 import XMonad.Util.Ungrab (unGrab)
+import XMonad.Actions.CycleWS (nextWS)
 
 main :: IO ()
 main = xmonad . ewmhFullscreen . ewmh . xmobarProp $ myConfig
@@ -24,10 +28,17 @@ main = xmonad . ewmhFullscreen . ewmh . xmobarProp $ myConfig
 myConfig =
   desktopConfig
     { terminal = "alacritty",
-      layoutHook = myLayout
+      layoutHook = myLayout,
+      handleEventHook = handleTimerEvent
     }
     `additionalKeysP` [ ("M-C-s", unGrab *> spawn "scrot -s"),
-                        ("M-f", spawn "vimb")
+                        ("M-f", spawn "vimb"),
+                        ("<XF86PowerOff>", spawn "systemctl suspend"),
+                        ("<XF86AudioRaiseVolume>", spawn "pamixer sset Master 10%+"),
+                        ("<XF86AudioLowerVolume>", spawn "pamixer sset Master 10%-"),
+                        ("<XF86AudioMute>", spawn "pamixer sset Master toggle"),
+                        ("<XF86MonBrightnessUp>", spawn "brightnessctl set +10%"),
+                        ("<XF86MonBrightnessDown>", spawn "brightnessctl set 10%-")
                       ]
 
 myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
