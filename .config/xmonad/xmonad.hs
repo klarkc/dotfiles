2e56a2e68f2aef29d03223b672fc81c6a4a14790
klarkc <walkerleite490@gmail.com>
2023-08-24 22:40:47 -0300

    feat(xmonad): add steam runner and grid

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -2,29 +2,53 @@ import Graphics.X11.ExtraTypes (xF86XK_PowerDown)
 import XMonad
   ( Default (def),
     Full (Full),
+    Layout (Layout),
     Mirror (Mirror),
     Tall (Tall),
     XConfig (keys, terminal),
+    XState (windowset),
+    className,
+    composeAll,
+    doIgnore,
+    doShift,
+    focusedBorderColor,
+    gets,
     handleEventHook,
     layoutHook,
+    liftX,
+    manageHook,
+    normalBorderColor,
+    sendMessage,
     spawn,
     startupHook,
     xmonad,
-    (|||), normalBorderColor, focusedBorderColor,
+    (-->),
+    (<+>),
+    (=?),
+    (|||),
   )
 import XMonad.Actions.CycleWS (nextWS)
 import XMonad.Actions.ShowText (flashText, handleTimerEvent)
 import XMonad.Config.Desktop (desktopConfig)
 import XMonad.Hooks.DynamicLog (xmobarProp)
 import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
+import XMonad.Hooks.ManageHelpers (doFullFloat)
 import XMonad.Hooks.StatusBar ()
 import XMonad.Hooks.StatusBar.PP ()
+import XMonad.Layout (JumpToLayout (JumpToLayout))
+import XMonad.Layout.Decoration (ModifiedLayout (ModifiedLayout))
+import XMonad.Layout.Grid (Grid (Grid))
+import XMonad.Layout.IfMax (IfMax (IfMax))
 import XMonad.Layout.Magnifier (magnifiercz')
+import XMonad.Layout.MultiToggle (single)
+import XMonad.Layout.MultiToggle.Instances (StdTransformers (FULL))
+import XMonad.Layout.Tabbed (shrinkText, simpleTabbed, tabbed, tabbedLeft, tabbedRight)
 import XMonad.Layout.ThreeColumns (ThreeCol (ThreeColMid))
-import XMonad.Layout.Tabbed (tabbedLeft, simpleTabbed, shrinkText, tabbed, tabbedRight)
+import XMonad.Layout.ToggleLayouts (ToggleLayout (ToggleLayout))
+import XMonad.StackSet (hidden, tag)
 import XMonad.Util.EZConfig (additionalKeys, additionalKeysP)
-import XMonad.Util.Ungrab (unGrab)
 import XMonad.Util.Themes (darkTheme, theme)
+import XMonad.Util.Ungrab (unGrab)
 
 main :: IO ()
 main = xmonad . ewmhFullscreen . ewmh . xmobarProp $ myConfig
@@ -46,7 +70,7 @@ myConfig =
     }
     `additionalKeysP` [ ("M-C-s", unGrab *> spawn "scrot -s"),
                         ("M-f", spawn "brave"),
-                        ("M-s", spawn "steam"),
+                        ("M-s", runSteam),
                         ("<XF86PowerOff>", spawn "systemctl suspend"),
                         ("<XF86AudioRaiseVolume>", spawn "pamixer sset Master 10%+"),
                         ("<XF86AudioLowerVolume>", spawn "pamixer sset Master 10%-"),
@@ -55,10 +79,17 @@ myConfig =
                         ("<XF86MonBrightnessDown>", spawn "brightnessctl set 10%-")
                       ]
 
-myLayout =  tiled |||  Full ||| Mirror tiled ||| tabbedRight shrinkText (theme darkTheme) ||| threeCol 
+myLayout =
+  tiled
+    ||| Full
+    ||| Mirror tiled
+    ||| Grid
   where
-    threeCol = magnifiercz' 1.3 $ ThreeColMid nmaster delta ratio
     tiled = Tall nmaster delta ratio
     nmaster = 1
     ratio = 2 / 3
     delta = 3 / 100
+
+runSteam = do
+  spawn "steam -windowed"
+  sendMessage $ JumpToLayout "Full"
