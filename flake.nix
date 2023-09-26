7cfb6a9de8e8ac78531eb4958014df14c6531850
klarkc <walkerleite490@gmail.com>
2023-09-26 20:09:42 -0300

    feat(flake): remove nixos-install-tools, add nixos-rebuild

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
               cachix
               nix-output-monitor
               nix-tree
-              nixos-install-tools
+              nixos-rebuild
             ];
           };
         });
