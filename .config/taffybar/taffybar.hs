56dbaecaa15cf72d3e88204a27c5c0a4a7bf38e1
Walker Leite <walkerleite@gmail.com>
2025-08-22 14:38:39 -0300

    feat: enable solo098

--- .config/taffybar/taffybar.hs
+++ .config/taffybar/taffybar.hs
@@ -99,7 +99,7 @@ myTaffybarConfig =
           }
       workspaces = workspacesNew myWorkspacesConfig
       mem = pollingGraphNew memCfg 1 memCallback
-      disk = liftIO $ fsMonitorNew 500 ["/", "/mnt/ext", "/tmp"]
+      disk = liftIO $ fsMonitorNew 500 ["/", "/tmp"]
       clock = textClockNewWith def
       layout = layoutNew def
       windowsW = windowsNew def
