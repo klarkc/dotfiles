5b892b9aca33b67d9a54bde496056d31519d415a
KlarkC <walkerleite490@gmail.com>
2026-05-12 13:46:37 -0300

    Split Fusion npm payload and Nix runtime wrapper

--- .config/nix/fusion-npm.nix
+++ .config/nix/fusion-npm.nix
@@ -1,52 +1,82 @@
-{ pkgs ? import <nixpkgs> { } }:
+let
+  nixpkgs = builtins.fetchTree {
+    type = "github";
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "ed67bc86e84e51d4a88e73c7fd36006dc876476f";
+    narHash = "sha256-62EWg6lI0qyzm7oAx5cAnGkLutvJsRBe0KkEW2JDZCE=";
+  };
 
+  pkgs = import nixpkgs {
+    system = builtins.currentSystem;
+  };
+
+  fusionNpmPayload = pkgs.stdenvNoCC.mkDerivation {
+    pname = "fusion-npm-payload";
+    version = "0.27.0";
+
+    nativeBuildInputs = with pkgs; [
+      cacert
+      nodejs
+    ];
+
+    outputHashAlgo = "sha256";
+    outputHashMode = "recursive";
+    outputHash = pkgs.lib.fakeHash;
+
+    buildCommand = ''
+      export HOME="$TMPDIR/home"
+      export npm_config_cache="$TMPDIR/npm-cache"
+      export npm_config_update_notifier=false
+      export npm_config_fund=false
+      export npm_config_audit=false
+
+      mkdir -p "$HOME" "$npm_config_cache" "$out"
+
+      npm install --global \
+        --prefix "$out" \
+        --cache "$npm_config_cache" \
+        --no-audit \
+        --no-fund \
+        @runfusion/fusion@0.27.0
+    '';
+  };
+
+  runtimePath = pkgs.lib.makeBinPath (with pkgs; [
+    coreutils
+    curl
+    docker-client
+    findutils
+    gawk
+    gh
+    git
+    gnugrep
+    gnused
+    nodejs
+    openssh
+    python3
+    tmux
+    uv
+  ]);
+in
 pkgs.stdenvNoCC.mkDerivation {
-  pname = "fusion-npm";
+  pname = "fusion-runtime";
   version = "0.27.0";
 
   nativeBuildInputs = with pkgs; [
-    cacert
     makeWrapper
-    nodejs
   ];
 
-  outputHashAlgo = "sha256";
-  outputHashMode = "recursive";
-  outputHash = pkgs.lib.fakeHash;
+  dontUnpack = true;
 
-  buildCommand = ''
-    export HOME="$TMPDIR/home"
-    export npm_config_cache="$TMPDIR/npm-cache"
-    export npm_config_update_notifier=false
-    export npm_config_fund=false
-    export npm_config_audit=false
+  installPhase = ''
+    mkdir -p "$out/bin" "$out/lib"
+    cp -a ${fusionNpmPayload}/lib/node_modules "$out/lib/"
 
-    mkdir -p "$HOME" "$npm_config_cache" "$out"
+    makeWrapper ${pkgs.nodejs}/bin/node "$out/bin/fusion" \
+      --prefix PATH : ${runtimePath} \
+      --add-flags "$out/lib/node_modules/@runfusion/fusion/dist/bin.js"
 
-    npm install --global \
-      --prefix "$out" \
-      --cache "$npm_config_cache" \
-      --no-audit \
-      --no-fund \
-      @runfusion/fusion@0.27.0
-
-    patchShebangs "$out/bin"
-
-    wrapProgram "$out/bin/fusion" \
-      --prefix PATH : ${pkgs.lib.makeBinPath (with pkgs; [
-        coreutils
-        curl
-        docker-client
-        findutils
-        gawk
-        gh
-        git
-        gnugrep
-        gnused
-        openssh
-        python3
-        tmux
-        uv
-      ])}
+    ln -s ${pkgs.tmux}/bin/tmux "$out/bin/tmux"
   '';
 }
